a30534fa2da0ae4157f148cdbdf65e80702f32a0
sherry <zsherry0803@gmail.com>
2019-08-21 10:25:50 -0400

    satus code

--- person-service/src/main/java/com/company/personservice/controller/PersonController.java
+++ person-service/src/main/java/com/company/personservice/controller/PersonController.java
@@ -3,6 +3,7 @@ package com.company.personservice.controller;
 import com.company.personservice.model.Person;
 import com.company.personservice.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,12 +18,14 @@ public class PersonController {
     }
 
     @RequestMapping(value = "/person/{name}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
     public List<Person> findByName(@PathVariable String name) {
         List<Person> list = personRepository.findByName(name);
         return list;
     }
 
     @RequestMapping(value = "/person/addperson", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.OK)
     public Person createPerson(@RequestBody Person person) {
         person = personRepository.save(person);
         return person;
